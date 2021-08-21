feb4fab5279e04025c012dae7eb1fdb51ac8dd4e
yuki550 <77430815+yuki550@users.noreply.github.com>
2021-08-21 13:50:51 +0900

    discord bot

--- discordbot.py
+++ discordbot.py
@@ -1,20 +1,49 @@
-from discord.ext import commands
-from os import getenv
-import traceback
+import discord
+import random
 
-bot = commands.Bot(command_prefix='/')
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+q = []
+@client.event
+async def on_message(message):
+    global q
+    if message.author.bot:
+        return
+    elif message.content.startswith('qadd'):
+        q.append(message.content[5:])
+        await message.add_reaction('👍')
+    elif message.content == 'qrand':
+        if(len(q) == 0):
+            await message.channel.send('問題がありません。')
+        else:
+            await message.channel.send(random.choice(q))
+    elif message.content == 'qclear':
+        q = []
+        await message.channel.send('初期化しました。')
+    elif message.content == 'qlist':
+#discordは2000文字以上のメッセージを送れません。
+        sub = 0
+        while True:
+            if sub + 1500 < len(str(q)):
+                await message.channel.send('```py\n' + str(q)[sub:sub + 1500] + '```')
+            elif sub < len(str(q)) - 8:
+                await message.channel.send('```py\n' + str(q)[sub:] + '```')
+            else:
+                break
+            sub += 1500
+    elif message.content.startswith('qrem'):
+        q.remove(message.content[5:])
+        await message.add_reaction('👍')
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+client.run(token)
 
 
 token = getenv('DISCORD_BOT_TOKEN')
